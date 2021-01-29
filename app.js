3b7474de7a6e246bd59259dee23c0410de9e6d2b
David Rodriguez <david@bsdadm.com>
2021-01-29 17:51:06 -0500

    init the needed modules variables & constants

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-
 /**
  * * App Name: Employee Management System (https://github.com/davidsaulrodriguez/employee-management-system)
- * * Copyright: Copyright (c) 2020 Keeper authors (https://github.com/davidsaulrodriguez/employee-management-system/graphs/master)
+ * * Copyright: Copyright (c) 2020 Employee Management System authors (https://github.com/davidsaulrodriguez/employee-management-system/graphs/master)
  * * Licensed under BSD 2 Clause (https://github.com/davidsaulrodriguez/employee-management-system/blob/master/LICENSE)
- * 
- * TODO: Build out the interactive CLI using Inquirer and MySQL.
  */
+require('dotenv').config();
+const initQuestions = require('./lib/questions');
+const inquirer = require('inquirer');
+const table = require('console.table');
+const mysql = require('mysql');
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const database = process.env.DB_NAME;
+const db = mysql.createConnection({
+  host,
+  user,
+  password,
+  database
+})
+
+let employeeRoles = [];
+let employeeList = [];
+let managerList = [];
